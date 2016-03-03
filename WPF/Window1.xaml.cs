faf46d1d73a4411bdf1d0d4d36fce03cf1c2cd65
thomsen67 <thomas@t-t.dk>
2016-03-03 10:49:21 +0100

    Add WPF project (not used)

---
+++ WPF/Window1.xaml.cs
@@ -0,0 +1,60 @@
+﻿/* 
+    Gekko Timeseries Software (www.t-t.dk/gekko).
+    Copyright (C) 2016, Thomas Thomsen, T-T Analyse.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program (see the file COPYING in the root folder).
+    Else, see <http://www.gnu.org/licenses/>.        
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        //TT: Not used at the moment: just trying to do the GUI in WPF...
+        public Window1()
+        {
+            InitializeComponent();
+            FlowDocument myFlowDoc = new FlowDocument();
+            
+            
+            // Add paragraphs to the FlowDocument.
+            Run run = new Run("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum");
+            //run.Foreground = new SolidColorBrush(Colors.Maroon);
+            run.FontFamily = new FontFamily("Courier");
+            run.FontSize = 11.5;
+            myFlowDoc.Blocks.Add(new Paragraph(new Run("Paragraph 1")));
+            myFlowDoc.Blocks.Add(new Paragraph(new Run("Paragraph 2")));
+            myFlowDoc.Blocks.Add(new Paragraph(run));
+            // Add initial content to the RichTextBox.
+            richTextBox1.Document = myFlowDoc;
+            
+        }
+    }
+}
