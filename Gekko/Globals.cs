c17844c520b24f88eb813f2bcb166aef5c0737db
Thomas Thomsen <thomas@t-t.dk>
2017-10-10 15:55:42 +0200

    Small change

--- Gekko/Globals.cs
+++ Gekko/Globals.cs
@@ -40,7 +40,7 @@ namespace Gekko
     /// </summary>
     public class Globals        
     {
-        public static string versionInternal = "d11";  // "d11";
+        public static string versionInternal = "d12";  // "d12";
 
         public static bool testFileChange = true;
 
