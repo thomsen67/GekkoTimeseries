3d53c22f582b56e082a73b4e252c28fd3ce06796
Thomas Thomsen <thomas@t-t.dk>
2018-09-26 11:48:29 +0200

    Backup

--- Gekko/Genr.cs
+++ Gekko/Genr.cs
@@ -10,8 +10,6 @@ namespace Gekko
     {
         public static GekkoTime globalGekkoTimeIterator = GekkoTime.tNull;
         public static int labelCounter;
-        public static readonly ScalarVal i23 = new ScalarVal(0d);
-        public static readonly ScalarVal i24 = new ScalarVal(1d);
         public static void ClearTS(P p)
         {
         }
@@ -24,47 +22,14 @@ namespace Gekko
             GekkoSmpl smpl = new GekkoSmpl(); O.InitSmpl(smpl, p);
 
             //[[splitSTART]]
-            p.SetText(@"¤1"); O.InitSmpl(smpl, p);
+            p.SetText(@"¤0"); O.InitSmpl(smpl, p);
 
-            Func<GraphHelper, string> print0 = (gh) =>
-            {
-                O.Prt o0 = new O.Prt();
-                labelCounter = 0; o0.guiGraphIsRefreshing = gh.isRefreshing;
-                o0.guiGraphPrintCode = gh.printCode;
-                o0.guiGraphIsLogTransform = gh.isLogTransform;
-                o0.prtType = "p";
-                ESeriesMissing r1_0 = Program.options.series_array_print_missing; ESeriesMissing r2_0 = Program.options.series_normal_print_missing; try
-                {
-                    O.HandleOptionBankRef1(o0.opt_bank, o0.opt_ref); O.HandleMissing1(o0.opt_missing);
-                    {
-                        List<int> bankNumbers = null;
-                        O.Prt.Element ope0 = new O.Prt.Element();
-                        ope0.labelGiven = new List<string>() { "lag¨(x+0,1)|[@2,2:4='lag',<931>,1:2]|[@10,12:12=')',<1178>,1:12]" };
-                        smpl = new GekkoSmpl(o0.t1.Add(-2), o0.t2);
-                        ope0.printCodesFinal = Program.GetElementPrintCodes(o0, ope0); bankNumbers = O.Prt.GetBankNumbers(null, ope0.printCodesFinal);
-                        for (int bankNumberI = 0; bankNumberI < bankNumbers.Count; bankNumberI++)
-                        {
-                            int bankNumber = bankNumbers[bankNumberI];
-                            smpl.bankNumber = bankNumber;
-                            ope0.variable[bankNumber] = Functions.lag(O.Smpl(smpl, i24), smpl, O.Add(smpl, O.Lookup(smpl, null, null, "x", null, null, false, EVariableType.Var, null), i23), i24);
-                            if (bankNumberI == 0) O.PrtElementHandleLabel(smpl, ope0);
-                        }
-                        smpl.bankNumber = 0;
-                        o0.prtElements.Add(ope0);
-                    }
-
-                }
-                finally
-                {
-                    O.HandleOptionBankRef2(); O.HandleMissing2(r1_0, r2_0);
-                }
-                o0.counter = 5;
-                o0.printCsCounter = Globals.printCs.Count - 1;
-                o0.Exe();
-                return o0.emfName;
-            };
-            Globals.printCs.Add(Globals.printCs.Count, print0);
-            print0(new GraphHelper());
+            O.Assignment o0 = new O.Assignment();
+            O.AdjustT0(smpl, -1);
+            IVariable ivTmpvar24 = O.Lookup(smpl, null, null, "#m", null, null, false, EVariableType.Var, null).extend(false, smpl, O.Lookup(smpl, null, null, "#m", null, null, false, EVariableType.Var, null));
+            O.AdjustT0(smpl, 1);
+            O.Lookup(smpl, null, null, "#m", null, ivTmpvar24, true, EVariableType.Var, o0)
+            ;
 
 
             //[[splitSTOP]]
