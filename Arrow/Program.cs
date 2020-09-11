60cc70447bf178d7fd32e8904e9451b8bed4a797
Thomas Thomsen <thomas@t-t.dk>
2020-09-11 19:08:33 +0200

    More experiments: this works for sending ADAMBK to R, but without date info as strings.

--- Arrow/Program.cs
+++ Arrow/Program.cs
@@ -17,13 +17,14 @@ namespace Arrow
     public class Program
     {
         public static string _fileName = @"c:\Thomas\Desktop\gekko\testing\test.arrow";
-                
+
         public static void Main(string[] args)
         {
-            //DataFrame project in .NET
-            //See this thread: https://github.com/dotnet/runtime/issues/24920
-            //Eric Erhardt from MS has been involved in arrow/C#
-            //https://devblogs.microsoft.com/dotnet/an-introduction-to-dataframe/    
+        //DataFrame project in .NET
+        //See this thread: https://github.com/dotnet/runtime/issues/24920
+        //Eric Erhardt from MS has been involved in arrow/C#
+        //https://devblogs.microsoft.com/dotnet/an-introduction-to-dataframe/    
+        https://devblogs.microsoft.com/dotnet/net-for-apache-spark-in-memory-dataframe-support/
             //See this: https://stackoverflow.com/questions/56231247/numpy-pandas-counterpart-in-net-or-netcore/56280314#56280314
             //or better this: https://www.nuget.org/packages/Microsoft.Data.Analysis/
             //github: https://github.com/dotnet/corefxlab/tree/master/src/Microsoft.Data.Analysis            
@@ -33,29 +34,33 @@ namespace Arrow
             //Compression with LZ4 should probably be the standard, but how to do this from C#? See https://ursalabs.org/blog/2020-feather-v2/
 
             // --------> example
-            //PrimitiveDataFrameColumn<DateTime> dateTimes = new PrimitiveDataFrameColumn<DateTime>("DateTimes"); // Default length is 0.
-            //PrimitiveDataFrameColumn<int> ints = new PrimitiveDataFrameColumn<int>("Ints", 3); // Makes a column of length 3. Filled with nulls initially
-            //StringDataFrameColumn strings = new StringDataFrameColumn("Strings", 3); // Makes a column of length 3. Filled with nulls initially
-            ////Append 3 values to dateTimes
-            //dateTimes.Append(DateTime.Parse("2019/01/01"));
-            //dateTimes.Append(DateTime.Parse("2019/01/01"));
-            //dateTimes.Append(DateTime.Parse("2019/01/02"));
-            //DataFrame df = new DataFrame(dateTimes, ints, strings); // This will throw if the columns are of different lengths            
-            //df[0, 1] = 10; // 0 is the rowIndex, and 1 is the columnIndex. This sets the 0th value in the Ints columns to 10
-            //// Modify ints and strings columns by indexing
-            //ints[1] = 100;
-            //strings[1] = "Foo!";
-            //df.Info();
-            //// Add 5 to Ints through the DataFrame
-            //df.Columns["Ints"].Add(5, inPlace: true);
-            //DataFrameRow row0 = df.Rows[0];
-            //for (long i = 0; i < df.Rows.Count; i++)
-            //{
-            //    DataFrameRow row = df.Rows[i];
-            //}
-            //// Filter rows based on equality
-            //PrimitiveDataFrameColumn<bool> boolFilter = df.Columns["Strings"].ElementwiseEquals("Bar");
-            //DataFrame filtered = df.Filter(boolFilter);       
+            DataFrame df = null;
+            if (true)
+            {
+                PrimitiveDataFrameColumn<DateTime> dateTimes = new PrimitiveDataFrameColumn<DateTime>("DateTimes"); // Default length is 0.
+                PrimitiveDataFrameColumn<int> ints = new PrimitiveDataFrameColumn<int>("Ints", 3); // Makes a column of length 3. Filled with nulls initially
+                StringDataFrameColumn strings = new StringDataFrameColumn("Strings", 3); // Makes a column of length 3. Filled with nulls initially
+                                                                                         //Append 3 values to dateTimes
+                dateTimes.Append(DateTime.Parse("2019/01/01"));
+                dateTimes.Append(DateTime.Parse("2019/01/01"));
+                dateTimes.Append(DateTime.Parse("2019/01/02"));
+                df = new DataFrame(dateTimes, ints, strings); // This will throw if the columns are of different lengths            
+                df[0, 1] = 10; // 0 is the rowIndex, and 1 is the columnIndex. This sets the 0th value in the Ints columns to 10
+                               // Modify ints and strings columns by indexing
+                ints[1] = 100;
+                strings[1] = "Foo!";
+                df.Info();
+                // Add 5 to Ints through the DataFrame
+                df.Columns["Ints"].Add(5, inPlace: true);
+                DataFrameRow row0 = df.Rows[0];
+                for (long i = 0; i < df.Rows.Count; i++)
+                {
+                    DataFrameRow row = df.Rows[i];
+                }
+                // Filter rows based on equality
+                PrimitiveDataFrameColumn<bool> boolFilter = df.Columns["Strings"].ElementwiseEquals("Bar");
+                DataFrame filtered = df.Filter(boolFilter);
+            }
 
             string s, s0, s1, s2, s3;
 
@@ -81,22 +86,42 @@ namespace Arrow
                 int t1 = 1998;
                 int t2 = 2079;
                 int n = t2 - t1 + 1;
-                int k = db.storage.Count;
-                
-                List<DataFrameColumn> list = new List<DataFrameColumn>(k);                
+                int k = db.storage.Count + 1;
 
-                StringDataFrameColumn indexColumn = new StringDataFrameColumn("time", n);
-                foreach (GekkoTime t in new GekkoTimeIterator(new GekkoTime(EFreq.A, t1, 1), new GekkoTime(EFreq.A, t2, 1)))
+
+
+                List<DataFrameColumn> list = new List<DataFrameColumn>(k);
+
+                StringDataFrameColumn indexColumn = null;
+
+                bool useTimeCol = false;
+                if (useTimeCol)
                 {
-                    indexColumn.Add<string>(t.super.ToString());
+                    //indexColumn = new ArrowStringDataFrameColumn("time");
+
+                    List<string> dates = new List<string>();
+                    int i2 = -1;
+                    foreach (GekkoTime t in new GekkoTimeIterator(new GekkoTime(EFreq.A, t1, 1), new GekkoTime(EFreq.A, t2, 1)))
+                    {
+                        i2++;
+                        dates.Add(t.super.ToString());
+                        //indexColumn.Add<string>(t.super.ToString());
+                        //indexColumn[i2] = t.super.ToString();
+                        //indexColumn[i2] = t.super.ToString();
+                    }
+                    //indexColumn = ArrowStringDataFrameColumn.Create("time", dates);
+                    //ArrowStringDataFrameColumn.Create<ArrowStringDataFrameColumn>("time", dates);
+
+                    //indexColumn = new ArrowStringDataFrameColumn("lkj");
+                    indexColumn = new StringDataFrameColumn("lkj", n);
+                    list.Add(indexColumn);
                 }
-                //list.Add(indexColumn);
-                
+
                 int counter = 0;
                 foreach (KeyValuePair<string, IVariable> kvp in db.storage)
                 {
                     counter++;
-                    PrimitiveDataFrameColumn<double> column = new PrimitiveDataFrameColumn<double>(kvp.Key, n);
+                    PrimitiveDataFrameColumn<double> column = new PrimitiveDataFrameColumn<double>(G.Chop_RemoveFreq(kvp.Key), n);
                     int i = -1;
                     foreach (GekkoTime t in new GekkoTimeIterator(new GekkoTime(EFreq.A, t1, 1), new GekkoTime(EFreq.A, t2, 1)))
                     {
@@ -105,35 +130,31 @@ namespace Arrow
                         //column.Add<double>(ts.GetDataSimple(t));
                         column[i] = ts.GetDataSimple(t);
                     }
-                    //df2.Add<PrimitiveDataFrameColumn<double>>(xx);
-                    //df2.Add<PrimitiveDataFrameColumn<double>>(list);
-                    //newColumns.Add(xx);
                     list.Add(column);
-                    //if (counter > 10) break;
                 }
                 DataFrame df777 = new DataFrame(list);
+                //df777 = df;
                 s1 = "Construct arrow took: " + (DateTime.Now - dt1).TotalMilliseconds / 1000d;
 
-                //DataFrame df = new DataFrame(new PrimitiveDataFrameColumn<int>("Foo", 10), new PrimitiveDataFrameColumn<int>("Bar", Enumerable.Range(1, 10)));
-                //RecordBatch recordBatch = new RecordBatch.Builder(new NativeMemoryAllocator(alignment: 64))
-                //.Append("Column A", false, col => col.Int32(array => array.AppendRange(Enumerable.Range(0, 10))))
-                //.Append("Column B", false, col => col.Float(array => array.AppendRange(Enumerable.Range(0, 10).Select(x => Convert.ToSingle(x * 2)))))
-                //.Append("Column C", false, col => col.String(array => array.AppendRange(Enumerable.Range(0, 10).Select(x => $"Item {x + 1}"))))
-                //.Append("Column D", false, col => col.Boolean(array => array.AppendRange(Enumerable.Range(0, 10).Select(x => x % 2 == 0))))
-                //.Build();
-                                               
-
+                RecordBatch recordBatch = null;
+                if (false)
+                {
+                    //DataFrame df = new DataFrame(new PrimitiveDataFrameColumn<int>("Foo", 10), new PrimitiveDataFrameColumn<int>("Bar", Enumerable.Range(1, 10)));
+                    recordBatch = new RecordBatch.Builder(new NativeMemoryAllocator(alignment: 64))
+                    .Append("_Column A", false, col => col.Int32(array => array.AppendRange(Enumerable.Range(0, n))))
+                    .Append("Column B", false, col => col.Float(array => array.AppendRange(Enumerable.Range(0, n).Select(x => Convert.ToSingle(x * 2)))))
+                    .Append("Column C", false, col => col.String(array => array.AppendRange(Enumerable.Range(0, n).Select(x => $"Item {x + 1}"))))
+                    .Append("Column D", false, col => col.Boolean(array => array.AppendRange(Enumerable.Range(0, n).Select(x => x % 2 == 0))))
+                    .Build();
+                    DataFrame xxx = DataFrame.FromArrowRecordBatch(recordBatch);
+                    WriteArrow(recordBatch);                    
+                }
 
                 dt1 = DateTime.Now;
-                //WriteArrow(recordBatch);
-                WriteArrow(df777.ToArrowRecordBatches().First());
-                //bool temp = task.Result; //actually runs it
+                IEnumerable<RecordBatch> rb = df777.ToArrowRecordBatches();
+                RecordBatch first = rb.First();                
 
-                //dt1 = DateTime.Now;
-                //var batches = df.ToArrowRecordBatches();
-                //WriteArrow(batches, _file);
-
-
+                WriteArrow(first);
                 s2 = "Write arrow took: " + (DateTime.Now - dt1).TotalMilliseconds / 1000d;
 
             }
