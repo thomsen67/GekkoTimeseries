5f4fe8eb33391949629a8f61b0340adb0f16af16
Thomas Thomsen <thomas@t-t.dk>
2022-06-27 13:28:33 +0200

    Backup

--- Gekko/Functions.cs
+++ Gekko/Functions.cs
@@ -2106,6 +2106,29 @@ namespace Gekko
             {
                 new Error("setdomains(): Expected array-series");
             }
+            List m_list = m as List;
+            if (m_list == null)
+            {
+                new Error("setdomains(): Expected list of strings");
+            }
+            string[] ss = Stringlist.GetListOfStringsFromListOfIvariables(m_list.list.ToArray());
+            if (ss == null)
+            {
+                new Error("setdomains(): Error regarding the list elements. These should be strings.");
+            }
+            if (x_series.dimensions != ss.Length)
+            {
+                new Error("setdomains(): The array-timeseries has " + x_series.dimensions + ", whereas the provided list of domain names has " + ss.Length + " dimensions.");
+            }
+            foreach (string s in ss)
+            {
+                if (s.StartsWith(Globals.symbolCollection.ToString()) || s == "*")
+                {
+                    //good
+                }
+                else new Error("setdomains(): Problem in domain name '" + s + "'. Only the string '*' or strings starting with '#' are allowed.");
+            }
+            x_series.meta.domains = ss;
         }
 
         public static IVariable isarrayseries(GekkoSmpl smpl, IVariable _t1, IVariable _t2, IVariable x)
