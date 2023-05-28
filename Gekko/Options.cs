23f809b49afeba5ec514d012cbf56c0b8f51afe9
Thomas Thomsen <thomas@t-t.dk>
2023-05-28 21:05:16 +0200

    Backup

--- Gekko/Options.cs
+++ Gekko/Options.cs
@@ -40,8 +40,8 @@ namespace Gekko
         public bool bugfix_readfast = true;        //not mentioned in help.
         public bool bugfix_missingignore = true;   //not mentioned in help, is set true for Gekko 3.1.16.
         public bool bugfix_sas = false;            //not mentioned in help (used by KNR). For prn writes vars and name/date with CAPS and inside "", q and m are written as for instance 202003 instead of 2020q3. For csv, numbers are F15.6 with 4 digits for exponent (normal is F15.8 with 2 digits for exponent). 
-        public bool bugfix_lhs_dollar = true;     //not mentioned in help, if lhs condition is a series, just skips the 0 (false) values instead of setting them to 0. On the rhs, they are always set to 0. Does not affect non-series conditions like set membership.
-        public bool bugfix_lhs_dollar_warning = true;  //not mentioned in help, set false in Gekko 3.2 (or maybe 3.2.2).
+        public bool bugfix_lhs_dollar = true;      //not mentioned in help, if lhs condition is a series, just skips the 0 (false) values instead of setting them to 0. On the rhs, they are always set to 0. Does not affect non-series conditions like set membership.
+        public bool bugfix_lhs_dollar_warning = true;  //see above...
         // ---
         //method options could look like the 2 following:
         public string collapse_method = "total";  //total|avg|first|last
