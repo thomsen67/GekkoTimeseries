3a2114042b1200e6ca13480f96692b1229d5824a
Thomas Thomsen <thomas@t-t.dk>
2024-04-10 10:08:57 +0200

    Bump to 3.1.19

--- Gekko/Properties/AssemblyInfo.cs
+++ Gekko/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.1.18.0")]
-[assembly: AssemblyFileVersion("3.1.18.0")]
+[assembly: AssemblyVersion("3.1.19.0")]
+[assembly: AssemblyFileVersion("3.1.19.0")]
