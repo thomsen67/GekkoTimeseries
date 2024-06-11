f91586e22a22da730f6047af147f9c04a4fc075f
Thomas Thomsen <thomas@t-t.dk>
2024-06-11 09:12:36 +0200

    All unit tests ok, except: _Test_AREMOS_ras() --> investigating _Test_Sheet_List --> known _Test_TraceResurrection --> known

--- Gekko/Globals.cs
+++ Gekko/Globals.cs
@@ -57,7 +57,6 @@ namespace Gekko
         public const bool traceUsesOrMayUseRealDataPeriod = true;  //for some traces, the real data period is used, so missings may truncate it. So if a command has actually run over some period, and it produces missings, its trace may completely disappear. In that case, the user just sees missing for some period, but cannot se the code line "producing" this missing. If this needs to be fixed, the varible acts as a pointer to these places.
         public const bool traceEndoRhsFix1 = true;   //fix hall or mirrors for x[%t] = x[%t] + ...
         public const bool traceEndoRhsFix2 = true;   //fix for  x = 1; x = x + 1; x = x + 1; ...
-        public const bool traceSimilarFix1 = true;        
 
         public static DayOfWeek weeklyWeekDayDefaultTsd = DayOfWeek.Friday;
         public static DayOfWeek weeklyFirstDayWhenPrinting = DayOfWeek.Monday;
