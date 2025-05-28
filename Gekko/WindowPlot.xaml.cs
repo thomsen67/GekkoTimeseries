2091fd26d0775db806eb5bd1b618cfbb3fe0a229
Thomas Thomsen <thomas@t-t.dk>
2025-05-28 03:18:35 +0200

    Backup

--- Gekko/WindowPlot.xaml.cs
+++ Gekko/WindowPlot.xaml.cs
@@ -379,7 +379,7 @@ namespace Gekko
 
         private void Button_search(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("One or more variables were found in a databank in position 2 or lower in the databank list (cf. the F2 window). This happens because databank searching is active: to switch databank searching off, use 'option databank search = no', in which case an error would have been issued instead. Beware of missing variables in the first-position databank.");
+            MessageBox.Show("One or more variables were found in a databank in position 2+ (that is, in position 2 or lower in the F2 window listing). This happens because databank searching is active: to switch databank searching off, use 'option databank search = no', in which case an error would have been issued instead. Beware of missing variables in the first-position databank. (The searching behavior does not affect how variables are found regarding the 'REF' position in the F2 window list.");
         }
 
         private void Button_saveas(object sender, RoutedEventArgs e)
