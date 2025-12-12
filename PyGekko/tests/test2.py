ecbd912d0a34af9c9f3718b3d39923bdf66a1450
Thomas Thomsen <thomas@t-t.dk>
2025-12-12 16:35:43 +0100

    Release 3.3.2 (and PyGekko 3.3.2.post1)

--- PyGekko/tests/test2.py
+++ PyGekko/tests/test2.py
@@ -1,4 +1,5 @@
 import pygekko as pg
+import pandas as pd
 t1 = 2021
 t2 = 2023
 pg.run("reset;")
@@ -7,3 +8,19 @@ pg.run("x1 = 2, 3, 4;")
 pg.run("x2 = 7, 5, 6;")
 pg.run("y = x1 + x2;")
 pg.run("prt x1, x2, x1/y;")
+
+import pyarrow.parquet as pq
+ 
+parquet_file = pq.ParquetFile("c:\\Thomas\\Desktop\\gekko\\testing\\makrobk.parquet")
+ 
+df0 = parquet_file.read_row_group(0).to_pandas().drop(columns=['date', 'period', 'value'])
+df1 = parquet_file.read_row_group(1).to_pandas()[['id', 'date', 'period', 'value']]
+df = df0.merge(df1[['id']], on='id', how='right')
+df[['date', 'period', 'value']] = df1[['date', 'period', 'value']].values
+print(df)
+
+ 
+print()
+print("Gekko parquet version:  " + parquet_file.metadata.metadata.get(b"parquet.design.version").decode("utf-8"))
+print("Gekko export timestamp: " + parquet_file.metadata.metadata.get(b"export.timestamp").decode("utf-8"))
+
