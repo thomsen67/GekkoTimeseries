0f73896a041e7aac500024a7258a4948979e91b3
Thomas Thomsen <thomas@t-t.dk>
2026-05-26 17:38:43 +0200

    Backup

--- Gekko/Globals.cs
+++ Gekko/Globals.cs
@@ -60,8 +60,8 @@ namespace Gekko
         //public static string dlink_dataFolder = G.CleanupFolderName(@"c:\Tools\Makrobk_kilde\2025_10_01\" + ident + "\\" + sub + @"\biver", false);
         //public static string dlink_blobsFolder = G.CleanupFolderName(@"c:\Tools\Makrobk_kilde\2025_10_01\_blobs", false);
 
-        public static string ident = "tth";
-        public static string sub = "test2";
+        public static string ident = "dsi"; // "tth";
+        public static string sub = "test"; // "test3";
         public static string dlink_programFolderGit = G.CleanupFolderName(@"k:\makrobk\" + ident + "\\" + sub, false);
         public static string dlink_programFolderRoot = G.CleanupFolderName(@"k:\makrobk\" + ident + "\\" + sub + @"\makrobk_grunddata", false);
         public static string dlink_programFolderRunning = G.CleanupFolderName(@"k:\makrobk\" + ident + "\\" + sub + @"\makrobk_grunddata\biver", false);
