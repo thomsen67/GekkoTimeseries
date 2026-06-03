34e976158428f64e45c0dc59d68ad7197db228df
Thomas Thomsen <thomas@t-t.dk>
2026-06-03 11:31:05 +0200

    This reproduces ADAM RAS DatopGek24, all 3 variants i=1,2,3. So old RAS, new RAS without #exo, and new RAS with #exo. See kqr_2026_06_03a.gcm to reproduce.

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -35428,10 +35428,10 @@ print(df2)
             I("option folder working = '" + Globals.ttPath2 + @"\regres\Ras';");
             for (int i = 0; i <= 2; i++)
             {
-                if (i == 1) continue; //For some reason this does not work, probably just some code in kqr.gcm. Never mind, i == 2 tests balance() really well.
+                //if (i == 1) continue; //For some reason this does not work, probably just some code in kqr.gcm. Never mind, i == 2 tests balance() really well.
                 I("writefile('options.txt', '" + i + "');"); //Just for safety
                 I("sys'del ras.gbk';"); //Just for safety                
-                I("run kqr;");
+                I("run kqr;"); //for this commit, see kqr_2026_06_03a.gcm, reproducing i=1,2,3.
                 I("read ras;");
                 I("read <ref> 'ras_datopgek24_2026-04-08.gbk';");
                 Globals.unitTestScreenOutput.Clear();
